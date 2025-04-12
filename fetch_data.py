ac71626bb12ba5445d8a43a0ae09892f65c72819
Admin <baskov_konstantin@mail.ru>
2025-04-12 14:52:31 +0300

    Добавил Логгирование Структурировал проект

--- fetch_data.py
+++
@@ -1,8 +0,0 @@
-import json
-
-# чтение конфиг файла GAS.ini
-with open(r'./GAS.ini', 'r') as f:
-    data = f.read()
-    cuv_length = float(data[data.find("Cuvette length=") + 15:data.find("Scans")].strip())
-    scans = int(data[data.find("Scans=") + 6:data.find("Resolution")].strip())
-    res = float(data[data.find("Resolution=") + 11:data.find("Spectra days")].strip())
